4dfea2f53eb3ceb76ec70abb86cf1d9b15fba946
Matheus Campos <mcquintao.qui@gmail.com>
2023-05-12 13:02:26 -0300

    chap2

--- chap2/jogo_de_advinhacao/src/main.rs
+++ chap2/jogo_de_advinhacao/src/main.rs
@@ -5,20 +5,17 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    // Mensagem de início do jogo
-    println!("Advinhe o número");
+    println!("Advinhe o número!");
 
-    let numero_secreto = rand::thread_rng().gen_range(1, 101);
+    let numero_secreto = rand::thread_rng().gen_range(1..=100);
 
-    //println!("O número secreto é: {}", numero_secreto);
+    // println!("O número secreto é {numero_secreto}");
 
     loop {
-        println!("Digite seu palpite");
+        println!("Digite o seu palpite");
 
-        // Craidno uma variável mutável
         let mut palpite = String::new();
 
-        // Lendo entrada do usuário
         io::stdin()
             .read_line(&mut palpite)
             .expect("Falha ao ler entrada");
@@ -26,24 +23,18 @@ fn main() {
         let palpite: u32 = match palpite.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Digite apenas números inteiros de 1 a 100!");
+                println!("Digite apenas números inteiros entre 1 e 100");
                 continue;
-            },
+            }
         };
 
-        let x = 5;
-        let y = 10;
-
-        println!("x = {x}, y = {y}");
-        println!("x = {}, y = {}", x, y);
-
-        println!("Você disse: {}", palpite);
+        println!("Você disse: {palpite}");
 
         match palpite.cmp(&numero_secreto) {
             Ordering::Less => println!("Muito baixo"),
             Ordering::Greater => println!("Muito alto"),
             Ordering::Equal => {
-                println!("Você acertou");
+                println!("Acertou!");
                 break;
             }
         }
